114940ce64ce0b19176d9ad910296af93fdbbd0b
Jake <55953689+jakecreely@users.noreply.github.com>
2025-01-05 19:58:38 +0000

    Cleaned up build script

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Variables
-MAIN_REPO_URL="https://github.com/jakecreely/volunteer-tracker-demo.git"
+REPO_URL="https://github.com/jakecreely/volunteer-tracker-demo.git"
 APP_DIR="volunteer-tracker-demo"
 
-# Clone the main repository
 if [ ! -d "$APP_DIR" ]; then
-  echo "Cloning main repository..."
-  git clone $MAIN_REPO_URL
+  echo "Cloning repo"
+  git clone $REPO_URL
 else
-  echo "Repository already exists. Pulling the latest changes..."
+  echo "Repo already exists. Pulling latest changes"
   cd $APP_DIR && git pull && cd ..
 fi
 
-# Build the frontend and backend folders
-echo "Preparing the frontend and backend..."
+
+echo "Extracting the frontend and backend..."
 cp -r $APP_DIR/client ./frontend
 cp -r $APP_DIR/server ./backend
